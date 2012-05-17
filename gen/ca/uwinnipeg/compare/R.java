6b2935df27efb3285a407a34ed67dcd77d6ade38
Garrett Smith <garrettwhsmith@gmail.com>
2012-05-17 13:01:20 -0500

    Created about page

--- gen/ca/uwinnipeg/compare/R.java
+++ gen/ca/uwinnipeg/compare/R.java
@@ -8,37 +8,60 @@
 package ca.uwinnipeg.compare;
 
 public final class R {
+    public static final class array {
+        public static final int about_developers=0x7f040000;
+        public static final int about_thanks=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int sample_0=0x7f020001;
-        public static final int sample_1=0x7f020002;
-        public static final int sample_2=0x7f020003;
-        public static final int sample_3=0x7f020004;
-        public static final int sample_4=0x7f020005;
-        public static final int sample_5=0x7f020006;
-        public static final int sample_6=0x7f020007;
-        public static final int sample_7=0x7f020008;
-        public static final int sample_image_1=0x7f020009;
-        public static final int sample_image_2=0x7f02000a;
-        public static final int sample_image_3=0x7f02000b;
-        public static final int sample_image_4=0x7f02000c;
-        public static final int sample_image_5=0x7f02000d;
-        public static final int sample_image_6=0x7f02000e;
-        public static final int sample_image_7=0x7f02000f;
-        public static final int sample_image_8=0x7f020010;
-        public static final int sample_image_9=0x7f020011;
+        public static final int ic_menu_add=0x7f020001;
+        public static final int ic_menu_close_clear_cancel=0x7f020002;
+        public static final int ic_menu_info_details=0x7f020003;
+        public static final int sample_0=0x7f020004;
+        public static final int sample_1=0x7f020005;
+        public static final int sample_2=0x7f020006;
+        public static final int sample_3=0x7f020007;
+        public static final int sample_4=0x7f020008;
+        public static final int sample_5=0x7f020009;
+        public static final int sample_6=0x7f02000a;
+        public static final int sample_7=0x7f02000b;
     }
     public static final class id {
-        public static final int gallery=0x7f050000;
-        public static final int imageView=0x7f050001;
+        public static final int about_app_name=0x7f070001;
+        public static final int about_app_version=0x7f070002;
+        public static final int about_developers=0x7f070004;
+        public static final int about_developers_title=0x7f070003;
+        public static final int about_icon=0x7f070000;
+        public static final int about_thanks=0x7f070006;
+        public static final int about_thanks_title=0x7f070005;
+        public static final int gallery=0x7f070007;
+        public static final int imageView=0x7f070008;
+        public static final int menu_about=0x7f07000e;
+        public static final int menu_cancel=0x7f07000d;
+        public static final int menu_create=0x7f07000c;
+        public static final int selection=0x7f07000b;
+        public static final int selection_image=0x7f07000a;
+        public static final int selection_view=0x7f070009;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int neighbourhood_select=0x7f030002;
+    }
+    public static final class menu {
+        public static final int neighbourhood_select=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int about_developers_title=0x7f050006;
+        public static final int about_thanks_title=0x7f050007;
+        public static final int app_about=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int app_versionName=0x7f050001;
+        public static final int menu_about=0x7f050005;
+        public static final int menu_cancel=0x7f050004;
+        public static final int menu_create=0x7f050003;
     }
     public static final class styleable {
         /** Attributes that can be used with a HelloGallery.
--- src/ca/uwinnipeg/compare/NeighbourhoodSelectionActivity.java
+++ src/ca/uwinnipeg/compare/NeighbourhoodSelectionActivity.java
@@ -2,20 +2,34 @@ package ca.uwinnipeg.compare;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Region;
 import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
 import android.graphics.drawable.shapes.Shape;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class NeighbourhoodSelectionActivity extends Activity {
   private Shape selectionShape; // The shape of the current neighbourhood
-  private Bitmap image;
+  private Rect selectionBounds; // A rectangle that store the size and position of the selection
+  private Bitmap image; // The image the neighbourhood belongs to
+  
+  private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+  private Uri fileUri;
 
   /**
    * Called when this Activity is first created.
@@ -26,50 +40,92 @@ public class NeighbourhoodSelectionActivity extends Activity {
     
     // Load image
     Resources res = getResources();
-    BitmapFactory bmf = new BitmapFactory();
-    image = bmf.decodeResource(res, R.drawable.sample_0);
+    image = BitmapFactory.decodeResource(res, R.drawable.sample_0);
+    
+    // Request image from camera
+    Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+    i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+    
+    startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     
     // Create shape
-    selectionShape = new OvalShape();
-    selectionShape.resize(200, 200);
+    selectionShape = new RectShape();
+    
+    // Create Bounds to fill entire image
+    int padding = 8;
+    int padWidth = image.getWidth()/padding;
+    int padHeight = image.getHeight()/padding;
+    selectionBounds = new Rect(padWidth, padHeight, image.getWidth()-padWidth, image.getHeight()-padHeight);
     
     // Create view
     SelectionView selView = new SelectionView(this);
+    
+    // Setup view
     selView.setImage(image);
     selView.setSelectionShape(selectionShape);
+    selView.setSelectionBounds(selectionBounds);
     
     // add view to activity
     setContentView(selView);
   }
+  
+  /**
+   * Create the menu.
+   */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.neighbourhood_select, menu);
+      return true;
+  }
+  
+  /**
+   * Respond to buttons presses.
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case R.id.menu_about:
+      showAbout();
+      return true;
+     default:
+       return super.onOptionsItemSelected(item);
+    }
+  }
+  
+  /**
+   * Displays the about dialog.
+   */
+  public void showAbout() {
+    Intent i = new Intent(this, AboutActivity.class);
+    startActivity(i);
+  }
 
   /**
    * 
-   * @author Garrett Smith
+   * The view which the image and currently selected neighbourhood is drawn.
    *
    */
   private static class SelectionView extends View {
-    private ShapeDrawable selectionShape; // The shape to be drawn as the current neighbourhood
+    private ShapeDrawable selection; // The shape to be drawn as the current neighbourhood
     private Bitmap image; // The image to draw as the background
     
-    private float rot;
-    
     private Paint imagePaint;
-    
-    private ShapeDrawable mDrawable;
 
     public SelectionView(Context context){
       super(context);
       setFocusable(true);	
 
       // Setup selection shape
-      selectionShape = new ShapeDrawable();
-      selectionShape.setBounds(100, 100, 300, 300);
+      selection = new ShapeDrawable();
       
       // Setup the paint for the selection shape (colour, stroke, etc.)
-      Paint selectionPaint = selectionShape.getPaint();
-      selectionPaint.setColor(0xff0066ff);
-      selectionPaint.setStrokeWidth(0);
-      selectionPaint.setStyle(Paint.Style.STROKE);
+      Paint selectionPaint = selection.getPaint();
+      
+      selectionPaint.setColor(0xff00ccff); // SEPERATE STYLE
+      selectionPaint.setStrokeWidth(2); // SEPERATE STYLE
+      selectionPaint.setStyle(Paint.Style.STROKE); // SEPERate STYLE
 
       imagePaint = new Paint();
     }
@@ -78,7 +134,14 @@ public class NeighbourhoodSelectionActivity extends Activity {
      * Set the shape being used to select a neighbourhood.
      */
     public void setSelectionShape(Shape s) {
-      selectionShape.setShape(s);
+      selection.setShape(s);
+    }
+    
+    /**
+     * Set the bounds of the selection.
+     */
+    public void setSelectionBounds(Rect r) {
+      selection.setBounds(r);
     }
 
     /**
@@ -93,9 +156,17 @@ public class NeighbourhoodSelectionActivity extends Activity {
 
       // Draw the image to the background
       canvas.drawBitmap(image, 75, 75, imagePaint);
+      canvas.translate(75, 75); // TMP
+      
+      // Dim unselected area
+      canvas.save();
+      canvas.clipRect(0, 0, canvas.getWidth(), canvas.getHeight());
+      canvas.clipRect(selection.getBounds(), Region.Op.DIFFERENCE); // NEED TO GENERALIZE TO ANY SHAPE
+      canvas.drawARGB(150,0,0,0);
+      canvas.restore();
       
       // Draw the shape of the current selection
-      selectionShape.draw(canvas);
+      selection.draw(canvas);
     }
 
   }
